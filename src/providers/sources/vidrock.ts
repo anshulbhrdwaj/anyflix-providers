ca65af5ebff1310bff8e3d2ca3ccdedba922e2d6
Umang Bhardwaj <umangbhardwaj0006@gmail.com>
2025-12-25 22:12:48 +0530

    fix: force node crypto to avoid browser polyfill

--- src/providers/sources/vidrock.ts
+++ src/providers/sources/vidrock.ts
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import crypto from 'node:crypto';
 
 import { flags } from '@/entrypoint/utils/targets';
 import { SourcererOutput, makeSourcerer } from '@/providers/base';
