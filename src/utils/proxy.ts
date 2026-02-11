e4fb8869ee096c587f6914f1e01e3eeef5c6f5e4
Umang Bhardwaj <umangbhardwaj0006@gmail.com>
2026-02-11 17:34:28 +0530

    Updated proxy url

--- src/utils/proxy.ts
+++ src/utils/proxy.ts
@@ -3,9 +3,9 @@ import { Stream } from '@/providers/streams';
 
 // https://anyflix-proxy.auralytics-in.workers.dev
 // Default proxy URL for general purpose proxying
-const DEFAULT_PROXY_URL = 'https://proxy.anyflix.fun';
+const DEFAULT_PROXY_URL = 'https://anyflix-proxy.auralytics-in.workers.dev';
 // Default M3U8 proxy URL for HLS stream proxying
-let CONFIGURED_M3U8_PROXY_URL = 'https://proxy.anyflix.fun';
+let CONFIGURED_M3U8_PROXY_URL = 'https://anyflix-proxy.auralytics-in.workers.dev';
 
 /**
  * Set a custom M3U8 proxy URL to use for all M3U8 proxy requests
